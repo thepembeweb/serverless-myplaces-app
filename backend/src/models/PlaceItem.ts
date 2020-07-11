ce86b479ff895b7c2ae49c8a2f92db21d396f120
Pemberai Sweto <pembeweb@yahoo.com>
2020-07-11 22:58:17 +0200

    feat: add project models

---
+++ backend/src/models/PlaceItem.ts
@@ -0,0 +1,9 @@
+export interface PlaceItem {
+  userId: string
+  placeId: string
+  createdAt: string
+  name: string
+  dueDate: string
+  done: boolean
+  attachmentUrl?: string
+}
---
+++ backend/src/models/PlaceUpdate.ts
@@ -0,0 +1,5 @@
+export interface PlaceUpdate {
+  name: string
+  dueDate: string
+  done: boolean
+}
